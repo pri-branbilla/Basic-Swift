96c6b086e5ce5964fbb48c9b1f901f0bcfe89182
Priscilla Nascimento <priscillans2712@gmail.com>
2020-02-09 23:59:40 -0300

    add value buttons function

--- Calculator/calculator/calculator/ViewController.swift
+++ Calculator/calculator/calculator/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var calcValue: UILabel!
+    
+    private func addText(value: String) {
+        if (calcValue.text == "0") {
+            calcValue.text = value;
+        } else {
+            calcValue.text! += value;
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func clearValue(_ sender: UIButton) {
+        calcValue.text = "0";
+    }
 
+    @IBAction func valueButton(_ sender: UIButton) {
+        addText(value: sender.currentTitle!)
+    }
 }
 
